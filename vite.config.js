4b257da4be4c6a73c69e183cacccbf91b38fc8ce
rybbl <rybble@msn.com>
2026-03-12 23:44:40 +0100

    mise a jour 12/03/2026 23:44:40,84

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,81 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      manifest: {
+        name: 'Mon Patrimoine',
+        short_name: 'Patrimoine',
+        description: 'Dashboard de suivi patrimonial personnel',
+        theme_color: '#0B1120',
+        background_color: '#0B1120',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        scope: '/patrimoine/',
+        start_url: '/patrimoine/',
+        lang: 'fr',
+        icons: [
+          {
+            src: '/patrimoine/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/patrimoine/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallback: '/patrimoine/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: { maxEntries: 10, maxAgeSeconds: 31536000 }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-webfonts',
+              expiration: { maxEntries: 30, maxAgeSeconds: 31536000 }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/api\.coingecko\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'coingecko-cache',
+              networkTimeoutSeconds: 5,
+              expiration: { maxEntries: 20, maxAgeSeconds: 300 }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/finnhub\.io\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'finnhub-cache',
+              networkTimeoutSeconds: 5,
+              expiration: { maxEntries: 20, maxAgeSeconds: 300 }
+            }
+          }
+        ]
+      },
+      devOptions: { enabled: false }
+    })
+  ],
   base: '/patrimoine/',
 })
